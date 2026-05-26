b1b2e32d727ec1660369e648b4075ede0e04acc7
kiieeaa <kiyaadinda5@gmail.com>
2026-05-26 10:25:48 +0700

    cek apakah antrian penuh

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -23,7 +23,9 @@ class Queues
         cin >> num;
         cout << endl;
 
-        
-    }
+        // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            
 
 }
