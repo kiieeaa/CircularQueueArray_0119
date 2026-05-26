6b69e9e264ef4ceead3a422711b0fe70feeaf09f
kiieeaa <kiyaadinda5@gmail.com>
2026-05-26 10:27:22 +0700

    queue overflow

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -26,6 +26,9 @@ class Queues
         // cek apakah antrian penuh
         if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
         {
+            cout << "\nQueue overflow\n";
+            return;
+        }
             
 
 }
